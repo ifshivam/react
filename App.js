e0db3d6fc1775ba225b72488340657221a695e7a
ifshivam <shivamrai0307@gmail.com>
2024-07-04 03:25:09 +0530

    creates-2 div in react

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
-const heading = React.createElement("h1",{},"Hello World from React!!");
+const heading =
+[
+    React.createElement("div",{id:"DIV1"},"Hello World from DIV1!!",),
+    React.createElement("div",{id:"DIV2"},"Hello World from DIV2!!")
+]
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
