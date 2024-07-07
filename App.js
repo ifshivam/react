209e9f7d1d2d76aab659246a844fca25ab8f09ad
ifshivam <shivamrai0307@gmail.com>
2024-07-08 01:14:34 +0530

    provides comment

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 const heading =
 [
-    React.createElement("div",{id:"DIV1"},"Hello World from DIV1!!",),
+    React.createElement("div",{id:"DIV1"},"Hello World from DIV1!!",), //these are the objects, it will be converted into html after rendering
     React.createElement("div",{id:"DIV2"},"Hello World from DIV2!!")
 ]
 const root = ReactDOM.createRoot(document.getElementById("root"));
