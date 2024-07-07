d0db899e792a4f53086476008e711787d549cda7
ifshivam <shivamrai0307@gmail.com>
2024-07-08 03:13:28 +0530

    injects component inside component

--- Component.js
+++ Component.js
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 
 //class based component -old way
 
+// just a casual another functional component
+const HeadingComponent = () => {
+  return <h1>Hello world from heading of functional component</h1>;
+};
+
 //functional component -new way
 const Component = () => {
-  return <h1>Hello world from functional component</h1>;
+  return (
+    <div>
+      <h1>Hello World from functional component</h1>
+      <HeadingComponent />
+    </div>
+  );
 };
 
 export default Component;
