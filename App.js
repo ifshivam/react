dda8cddd1f62fc176febb860b5e70e58456de19f
ifshivam <shivamrai0307@gmail.com>
2024-07-08 03:03:28 +0530

    creates functional component

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Component from "./Component"
 
 //reactObj->JS->HTML(during rendering)
-const heading = [
-  React.createElement("div", { id: "DIV1" }, "Hello World from DIV1!!"), //these are the objects, it will be converted into html after rendering
-  React.createElement("div", { id: "DIV2" }, "Hello World from DIV2!!"),
-];
+// const heading = [
+//   React.createElement("div", { id: "DIV1" }, "Hello World from DIV1!!"), //these are the objects, it will be converted into html after rendering
+//   React.createElement("div", { id: "DIV2" }, "Hello World from DIV2!!"),
+// ];
 
 // JSX style - JSX is not html in side JS but it is HTML or XML like syntax
 //jsx->reactObj->JS->HTML(during rendering) -by babel
-const jsxheading = <h1 id='jsx-heading'>Hello world in JSX</h1>;
+
+// const jsxheading = <h1 id='jsx-heading'>Hello world in JSX</h1>;
+const App =()=>{
+    return ( <div>
+        <h1>Hello World from App.js</h1>
+        <Component/>
+    </div>);
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<App/>);
---
+++ Component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//class based component -old way
+
+//functional component -new way
+const Component = () => {
+  return <h1>Hello world from functional component</h1>;
+};
+
+export default Component;
