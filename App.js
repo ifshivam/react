baeed1e7d358061e1e501fb96ea4c366bf9414a7
ifshivam <shivamrai0307@gmail.com>
2024-07-08 01:47:50 +0530

    provides comment

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//reactObj->JS->HTML(during rendering)
 const heading =
 [
     React.createElement("div",{id:"DIV1"},"Hello World from DIV1!!",), //these are the objects, it will be converted into html after rendering
     React.createElement("div",{id:"DIV2"},"Hello World from DIV2!!")
 ]
 
-// JSX style - JSX is not html in side JS but it is HTML or XML like syntax
+// JSX style - JSX is not html in side JS but it is HTML or XML like syntax 
+//jsx->reactObj->JS->HTML(during rendering) -by babel
 const jsxheading = <h1 id="jsx-heading">Hello world in JSX</h1> 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxheading);
