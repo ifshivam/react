34f26bcb1f89e0938a1de9b7ed7c89afcef936a6
ifshivam <shivamrai0307@gmail.com>
2024-07-08 01:35:18 +0530

    use jsx

--- App.js
+++ App.js
@@ -6,5 +6,8 @@ const heading =
     React.createElement("div",{id:"DIV1"},"Hello World from DIV1!!",), //these are the objects, it will be converted into html after rendering
     React.createElement("div",{id:"DIV2"},"Hello World from DIV2!!")
 ]
+
+// JSX style - JSX is not html in side JS but it is HTML or XML like syntax
+const jsxheading = <h1 id="jsx-heading">Hello world in JSX</h1> 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxheading);
